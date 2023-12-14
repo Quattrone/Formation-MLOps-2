e95baf36f5743f69045abb9520cf371979fbf852
Michele <quattrone@gmail.com>
2023-12-14 09:35:57 +0000

    try dags

--- dags/.ipynb_checkpoints/predict-checkpoint.py
+++ dags/.ipynb_checkpoints/predict-checkpoint.py
@@ -9,7 +9,6 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__)))) 
 
 from dags.config import GENERATED_DATA_PATH, DATA_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
-from formation_indus_ds_avancee.train_and_predict import predict_with_io
 
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
@@ -23,15 +22,10 @@ def predict():
                                  training_mode=False)
         return features_path
 
-    @task
-    def predict_with_io_task():
-        features_path = os.path.join(DATA_FOLDER, f'prepared_features_{datetime.now()}.parquet')
-        predict_with_io(feature_path = features_path)
     # Start completing predict task
     # predict = PythonOperator()
-    
     # End completing predict task
-    
+
     # feature_path = prepare_features_with_io_task()
     # predict_with_io_task(feature_path=feature_path)
 
